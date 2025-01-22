4f34f916be1c698894c922d86ca840951a9593ae
Leonardo Santoro <leonardovincenzo.santoro@gmail.com>
2025-01-22 12:37:58 +0100

    tru

--- functions/tru.py
+++ functions/tru.py
@@ -1,25 +1,25 @@
 
-
 from modules import *
-
 from functions.tools import *
 from functions.TestFuns import *
 from models import models_classes as models
 from functions.run import *
+from functions.tru import *
+import pandas as pd  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
+import seaborn as sns  # type: ignore
 
 
-from matplotlib import pyplot as plt # type: ignore
-from matplotlib.ticker import ScalarFormatter # type: ignore
-import seaborn as sns # type: ignore
 
+
+title_dict = {'isotropic_different_means' : 'Isotropic Gaussian data with different means',
+              'isotropic_vs_DiagSpiked' : 'Isotropic and diagonal spiked Gaussian data',}
 
 def sample_test_vals(sample_X, sample_Y, n, test_names, kappa_K = 1e6, kernel = None ):
     X = sample_X(n)
     Y = sample_Y(n)  
     pooled = np.vstack([X, Y])
-
     values  = []
-        
     for test_name in test_names:
         if test_name in all_CovKerEmb_tests:
             if kernel is None:
@@ -29,7 +29,6 @@ def sample_test_vals(sample_X, sample_Y, n, test_names, kappa_K = 1e6, kernel = 
                 kernel_matrix  = np.exp( - pairwise_dists / bandwidth)
             else:
                 kernel_matrix  = kernel(pooled, pooled)  
-
             kxx = kernel_matrix[:n, :n]
             kxy = kernel_matrix[:n, n:]
             kyy = kernel_matrix[n:, n:]
@@ -41,13 +40,12 @@ def sample_test_vals(sample_X, sample_Y, n, test_names, kappa_K = 1e6, kernel = 
             values.append(KNN_two_sample_test(X,Y).obs_value)
         if test_name == 'FR':
             values.append(FR_two_sample_test(X,Y).obs_value)
-        if test_name == 'MPZ':
-            values.append(MPZ_two_sample_test(X,Y).obs_value)
+        if test_name == 'HT':
+            values.append(HT_two_sample_test(X, Y, k=10).obs_value)
     return values
 
 
-def H0_H1(n,d, _model_,test_names, kappa_K, num_reps, kernel = None, NUM_CORES=4):
-
+def H0_H1(n,d, _model_,test_names, kappa_K, num_reps, kernel = None, NUM_CORES=8):
     # sample from null 
     iter_args = [(_model_(d).sample_X, _model_(d).sample_X, n, test_names, kappa_K, kernel) for _ in range(num_reps)]
     null_vals = Parallel(n_jobs=NUM_CORES)(delayed(sample_test_vals)(*args) for args in iter_args)
@@ -57,26 +55,3 @@ def H0_H1(n,d, _model_,test_names, kappa_K, num_reps, kernel = None, NUM_CORES=4
     alternative_vals = Parallel(n_jobs=NUM_CORES)(delayed(sample_test_vals)(*args) for args in iter_args)
 
     return null_vals, alternative_vals
-
-def plot_H0_H1(null_vals, alternative_vals, test_names, n, d, kappa_K= 'not given'):
-    # plot results
-    fig, axs = plt.subplots(1,len(test_names), figsize=(15, 2)); axs = axs.ravel()
-        
-    fig.suptitle(f'n={n}, d={d}, $\kappa$={kappa_K}', y=1.3)
-    for ax, test in zip(axs, test_names):
-        fromNULL = np.array(null_vals[test])
-        fromALTERNATIVE = np.array(alternative_vals[test])
-        sns.histplot(x = fromNULL, ax=ax, color='b', label = 'null')
-        sns.histplot(x = fromALTERNATIVE, ax=ax, color='r', label = 'alternative')
-
-        rthresh = np.quantile(fromNULL, .95)
-        power = np.mean(fromALTERNATIVE > rthresh); level = np.mean(fromNULL > rthresh)
-        ax.axvline(x=rthresh, color='g', linestyle='--', label = 'threshold', alpha =.9)
-
-        ax.set_title(f'{test}\npower: {power:.2f} | level: {level:.2f}', y=1.1)
-        ax.legend();
-        ax.xaxis.set_major_formatter(ScalarFormatter(useOffset=False)); ax.ticklabel_format(style='plain', axis='x')
-        for label in ax.get_xticklabels():
-            label.set_rotation(90)
-    plt.show()
-    return None
--- sims_perm.py
+++ sims_perm.py
@@ -6,11 +6,11 @@ from functions.run import *
 
 
 # --------- set parameters ---------------------------------------------------------------------------------------------------------------------
-NUM_CORES = 10
-n = 75
-n_d_values = [(n, 25),(n, 50),(n, 250),(n, 500),(n, 1000)]
-n = 125
-n_d_values += [(n, 25),(n, 50),(n, 250),(n, 500),(n, 1000)]
+NUM_CORES = 72
+n = 50
+n_d_values = [(n, 25),(n, 50),(n, 250),(n, 500),(n, 1000),(n, 1500)]
+n = 100
+n_d_values += [(n, 25),(n, 50),(n, 250),(n, 500),(n, 1000),(n, 1500)]
 
 num_permutations = 500
 N_iters = 100
@@ -24,25 +24,25 @@ test_names = ['FH', 'MMD', 'KNN', 'FR', 'HT']
 lsmodels = []
 
 _model_ = models.isotropic_different_means
-model_params = {'mu' : 1.5, 'numDiffLocs' :  20}
+model_params = {'mu' : 1.3, 'numDiffLocs' :  20}
 lsmodels.append((_model_ , model_params))
 
 _model_ = models.isotropic_vs_DiagSpiked
-model_params = {'num_spikes' : 10,  'spike_value' : 10}
+model_params = {'num_spikes' : 10,  'spike_value' : 5}
 lsmodels.append((_model_ , model_params))
 
 
 # --------- run -------------------------------------------------------------------------------------------------------------------------------
 def run_parallel(n, d, _model_, model_params, test_names, kernel_name, kernel_bandwith,  kappa_K, num_permutations, N_iters):
     iter_args = [(n, d, _model_,model_params, test_names, kernel_name, kernel_bandwith, kappa_K, num_permutations) for _ in range(N_iters)]
-    results = Parallel(n_jobs=NUM_CORES)(delayed(run_iteration)(*args) for args in tqdm(iter_args, position=1, leave=False))
+    results = Parallel(n_jobs=NUM_CORES)(delayed(run_iteration)(*args) for args in iter_args)
     return results
 
 
 lsout = [] 
 for _model_, model_params in lsmodels:
     results = []
-    for n, d in tqdm(n_d_values,position=0, leave=True):
+    for n, d in n_d_values:
         decisions_list = run_parallel(n, d, _model_, model_params, test_names, kernel_name, kernel_bandwith, kappa_K, num_permutations, N_iters)
         results.append(decisions_list)
     out = []
@@ -66,6 +66,9 @@ for _model_, model_params in lsmodels:
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import os
+
+
 title_dict = {'isotropic_different_means' : 'Isotropic Gaussian data with different means',
               'isotropic_vs_DiagSpiked' : 'Isotropic and diagonal spiked Gaussian data',}
 for file in lsout:
@@ -84,6 +87,11 @@ for file in lsout:
         ax.set_ylim(0, 1.025)
         ax.set_xscale('log') 
         plt.tight_layout()
-    fig.savefig('figures/simulations_{}.png'.format(file.split('/')[1]))
 
+
+	
+    output_dir = f'figures/{ts}'
+    os.makedirs(output_dir, exist_ok=True)
+    fig.savefig(f'{output_dir}/simulations_{file.split("/")[1]}.png')
+    
 # --------- end -------------------------------------------------------------------------------------------------------------------------------
